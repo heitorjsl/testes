d879fcaa71e3d0b9659a61b3c17bf75253574d47
𝔥𝔢𝔦𝔱𝔬𝔯 <heitorj6126@gmail.com>
2025-02-28 12:44:52 -0300

    Add files via upload

---
+++ teste calculadora/calculadora.py
@@ -0,0 +1,4 @@
+import testecalc
+print(testecalc.somar(5,5))
+print(testecalc.subtracao(2,1))
+print(testecalc.multiplicacao(5,2))
---
+++ teste calculadora/testecalc.py
@@ -0,0 +1,12 @@
+def somar (a,b):
+     return a+b
+print(somar(5,5))
+ 
+def subtracao (a,b):
+    return a-b
+print(subtracao(11,1))
+
+def multiplicacao(a,b):
+     return a*b
+print(multiplicacao(5,2))
+
