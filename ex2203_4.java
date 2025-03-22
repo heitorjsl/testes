60a048ec8812c6d28a5583b28601c5b708a52b70
𝔥𝔢𝔦𝔱𝔬𝔯 <heitorj6126@gmail.com>
2025-03-22 17:02:47 -0300

    Add files via upload

--- ex2203_4.java
+++ ex2203_4.java
@@ -1,13 +1,23 @@
 import javax.swing.JOptionPane;
 public class ex2203_4 {
-    public
-    double dia=Integer.parseInt(JOptionPane.showInputDialog("Digite numero correspondente a semana"));
-    string diasemana;
-    if(dia== 1){
+
+    public static void main(String[] args) {
+        double a, b, c,d,y1,y2;
+        a=Double.parseDouble(JOptionPane.showInputDialog("digite valor A"));
+        b=Double.parseDouble(JOptionPane.showInputDialog("digite valor B"));
+        c=Double.parseDouble(JOptionPane.showInputDialog("digite valor C"));
         
+        d=b*b-4*a*c;
+       
+        if (d<0){
+            JOptionPane.showMessageDialog(null,"nn existem raizes reais");
+        }else if (d==0){
+            y1= -b / (2 * a);
+            JOptionPane.showMessageDialog(null,"a raiz é"+ y1);
+        }else{
+            y1=(-b + Math.sqrt(d))/ (2 * a);
+            y2=(-b - Math.sqrt(d))/ (2*a);
+            JOptionPane.showMessageDialog(null, "As raízes reais são: \n y1 = " + y1 + "\n y2 = " + y2);
+        }       
     }
-
-
-
-    
 }
